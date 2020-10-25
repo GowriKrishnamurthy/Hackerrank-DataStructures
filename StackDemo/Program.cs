7234b3c26e84bb9ef594db14a6f3941e63ce7e70
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-10-25 18:05:35 +1100

    Stack - Balanced Brackets

---
+++ StackDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StackDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the # of strings.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the strings.");            
+            for (int i = 0; i < num; i++)
+            {
+                string s = Console.ReadLine();
+                string result = BalancedBrackets.IsBalanced(s);
+                Console.WriteLine(result);
+            }
+            
+        }
+    }
+}
